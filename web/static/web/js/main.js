01ca0efa585b1163e23361fa897469c56972886c
Ainsley Escorce-Jones <me@ains.co>
2014-05-23 16:34:42 +0100

    Completed initial frontend merge

--- web/static/web/js/main.js
+++ web/static/web/js/main.js
@@ -1,1 +1,31 @@
+$(document).ready(function() {
+	var $wittlist = $('.wittlist');
 
+	$wittlist.isotope({
+	  itemSelector: '.card-wrapper',
+	  layoutMode: 'fitRows',
+	  transitionDuration: '0.65s',
+	  getSortData: {
+	  	wittlWeight: function(elem) {
+	  		return parseInt($(elem).children('.card').data('wittlWeight'));
+	  	}
+	  }
+	});
+
+	$('body').on('keypress', function(e) {
+		if(e.which == 13) {
+			$wittlist.isotope({sortBy: 'wittlWeight'});
+		}
+	});
+
+	$('.wittl .reveal').hover( 
+		function(e) {
+			console.log('what is happ');
+			$(this).closest('.wittl').addClass('opened');
+		});
+
+	$('.wittl').on('mouseleave', function(e) {
+		console.log('enning');
+		$(this).closest('.wittl').removeClass('opened');
+	});
+});
