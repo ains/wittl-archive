c4156d9748c07db8141402b95cd9bd311903936e
ilovepjs <server@fake.co>
2014-05-23 13:02:47 +0100

    implemented weather, nearby, distance_from comparator

---
+++ comparator/weather.py
@@ -0,0 +1,14 @@
+from comparator.base import BaseComparator
+from partner_api.wunderground import WundergroundAPI
+
+
+class WeatherComparator(BaseComparator):
+    NAME = 'weather'
+    REQUIRED_ATTRIBUTES = ("latitude", "longitude")
+    TRIGGERS = [r"distance from (?P<destination_location>.*)"]
+    TITLE = "Weather at destination"
+        
+    def score(self, latitude, longitude, ideal_temperature, date):
+        w = WundergroundAPI()
+        weather_at_destination = w.get_weather(latitude, longitude, date)
+        return abs(weather_at_destination - ideal_temperature)
