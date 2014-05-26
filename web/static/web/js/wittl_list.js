8cbfb86831205cba685f23ef8ea8165fade37fee
Ainsley Escorce-Jones <me@ains.co>
2014-05-26 14:21:34 +0100

    Started work on separating out comparators

---
+++ web/static/web/js/wittl_list.js
@@ -0,0 +1,46 @@
+var initializeSorting = function (scoringData) {
+    var calculateScore = function (cardID, wittls) {
+        return _.reduce(wittls, function (acc, wittl, index) {
+            return acc + (scoringData[cardID][wittl] * (1 / Math.pow(2, index + 1)));
+        }, 0);
+    }
+
+    var $wittlist = $('.wittlist');
+    $wittlist.isotope({
+        itemSelector: '.card-wrapper',
+        layoutMode: 'fitRows',
+        transitionDuration: '0.65s',
+        getSortData: {
+            wittlWeight: function (elem) {
+                return parseInt($(elem).children('.card').data('wittl-weight'));
+            }
+        }
+    });
+
+    var panelList = $('.wittls');
+    console.log(panelList);
+    panelList.sortable({
+        handle: 'header',
+        update: function () {
+            //Get all wittl names
+            var wittls = $('.wittl').map(function (i, e) {
+                return $(e).data('wittl-name');
+            });
+
+            //Update cards with their new weights
+            $(".card").each(function (i, card) {
+                var cardID = $(card).data('id');
+                var newWeight = calculateScore(cardID, wittls);
+                $(card).data('wittl-weight', newWeight);
+            });
+
+            //Shuffle the deck!
+            $wittlist.isotope('updateSortData')
+            $wittlist.isotope({sortBy: 'wittlWeight'});
+        }
+    });
+
+    $('.wittl header').click(function (e) {
+        $(this).closest('.wittl').toggleClass('opened');
+    });
+};
