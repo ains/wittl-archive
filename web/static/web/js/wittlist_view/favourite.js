27b7ace015b937497c4098bb73ecadc590ccba2e
Ainsley Escorce-Jones <me@ains.co>
2014-06-06 17:29:34 +0100

    autoformatted

--- web/static/web/js/wittlist_view/favourite.js
+++ web/static/web/js/wittlist_view/favourite.js
@@ -1,11 +1,11 @@
-$(function() {
-	$('.wittlist').on('click', '.favourite', function (e) {
-		var list_item_id = $(this).parents(".card").data("list-item-id");
+$(function () {
+    $('.wittlist').on('click', '.favourite', function (e) {
+        var list_item_id = $(this).parents(".card").data("list-item-id");
         var csrftoken = $.cookie('csrftoken');
         var favourite = $(this).children("i");
-        $.post("/api/v1/list-items/" + list_item_id + "/toggle_favourite/", {"csrfmiddlewaretoken" : csrftoken}, function() {
+        $.post("/api/v1/list-items/" + list_item_id + "/toggle_favourite/", {"csrfmiddlewaretoken": csrftoken}, function () {
             favourite.toggleClass('icon_star_alt icon_star');
         });
         e.stopPropogation();
-	});
+    });
 });
