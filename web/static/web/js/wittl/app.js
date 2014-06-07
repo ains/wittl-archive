f609392d6975de355898f912886aa2c06ec19fe6
Ainsley Escorce-Jones <me@ains.co>
2014-06-07 16:10:32 +0100

    started work on wittlist view angular

--- web/static/web/js/wittl/app.js
+++ web/static/web/js/wittl/app.js
@@ -1,5 +1,8 @@
 var wittl = angular.module('wittl', [
-	'listsService',
-	'listsController'
+    'listsService',
+    'listsController'
 ]);
 
+wittl.controller('ListItemsCtrl', ['$scope', 'Lists', function ($scope, Lists) {
+    $scope.list = Lists.get({listID: 1});
+}]);
