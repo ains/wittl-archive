3db9422f2034245571c902606e2fdd5a8ced6f2a
Ainsley Escorce-Jones <me@ains.co>
2014-06-13 10:04:08 +0100

    Removed dead code
    Pruned code which is now in the API

--- web/views.py
+++ web/views.py
@@ -1,17 +1,10 @@
-import json
-from django.views.decorators.csrf import csrf_exempt
-import comparator
-
-from collections import defaultdict
-
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
-from django.views.decorators.http import require_POST, require_GET
-from django.http import HttpResponse, HttpResponseServerError, HttpResponseRedirect, HttpResponseForbidden
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse, HttpResponseRedirect
 
-from models import List, ListItem, ListComparator, ListForm
-from importer import get_importer_for_url
+from models import List, ListForm
 
 
 def index(request):
@@ -52,46 +45,6 @@ def list_view(request, list_id):
     }
     return render(request, "list/view.html", render_data)
 
-@login_required
-def all_comparators(request):
-    def get_comparator_data(comparator_class):
-        return {
-            'name': comparator_class.NAME,
-            'required_attributes': comparator_class.REQUIRED_ATTRIBUTES
-        }
-
-    comparator_json = json.dumps(map(get_comparator_data, comparator.all_comparators.values()))
-    return HttpResponse(comparator_json)
-
-
-@login_required
-@csrf_exempt
-@require_POST
-def save_comparator_settings(request):
-    comparator_id = request.POST['comparator_id']
-    new_configuration = request.POST['configuration']
-    comparator = get_object_or_404(ListComparator, id=comparator_id)
-    comparator.configuration = new_configuration
-    comparator.save()
-
-    return HttpResponse("ok")
-
-
-@login_required
-@csrf_exempt
-@require_POST
-def update_wittl_order(request, list_id):
-    list = get_object_or_404(List, id=list_id)
-    if list.user_invited(request.user):
-        wittl_ids = request.POST.getlist("wittl_ids[]")
-        for i, id in enumerate(wittl_ids):
-            wittl = ListComparator.objects.get(pk=int(id))
-            wittl.order = i
-            wittl.save()
-
-        return HttpResponse("ok")
-
-    return HttpResponseForbidden()
 
 @login_required
 def favourite_list(request):
