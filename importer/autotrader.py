9f236fc48bed4a5c578eb677dbdb5b770fab98fd
Ainsley Escorce-Jones <me@ains.co>
2014-06-15 22:09:17 +0100

    semi-working auto trader

--- importer/autotrader.py
+++ importer/autotrader.py
@@ -8,23 +8,20 @@ class AutoTraderImporter(BaseImporter):
     NAME = "autotrader"
     URL_PATTERNS = [r"https?:\/\/(www.)?autotrader.co.uk\/.*?"]
 
-    @staticmethod
-    def meta_getter(soup):
-        def property_getter(property_name):
+    class MetaGetter():
+        def __init__(self, soup):
+            self.soup = soup
+            pass
+
+        def property(self, property_name):
             h = HTMLParser.HTMLParser()
-            meta_tag = soup.find("meta", attrs={"property": property_name})
+            meta_tag = self.soup.find("meta", attrs={"property": property_name})
             return h.unescape(meta_tag["content"]) if meta_tag is not None else ""
 
-        def name_getter(name_name):
+        def name(self, name_name):
             h = HTMLParser.HTMLParser()
-            meta_tag = soup.find("meta", attrs={"name": name_name})
+            meta_tag = self.soup.find("meta", attrs={"name": name_name})
             return h.unescape(meta_tag["content"]) if meta_tag is not None else ""
-
-        def property_getter():
-            return property_getter
-
-        def name_getter():
-            return name_getter
 
 
     def extract_attributes(self, page):
@@ -33,7 +30,7 @@ class AutoTraderImporter(BaseImporter):
         soup = BeautifulSoup(page)
 
         table_attributes = {
-            'Urban mpg': 'urban_mpg', 'Extra Urban mpg': 'extra_urban_mpg', 'Average mpg': 'mpg', 
+            'Urban mpg': 'urban_mpg', 'Extra Urban mpg': 'extra_urban_mpg', 'Average mpg': 'mpg',
             'CO<sub>2</sub> emissions': 'CO2_emissions', 'Annual Tax': 'annual_tax',
             'Engine power': 'engine_power', 'Acceleration (0-62mph)': 'accleration',
             'Wheel drive': 'wheel_drive', 'Engine size': 'engine_size',
@@ -50,8 +47,8 @@ class AutoTraderImporter(BaseImporter):
                 attributes[internal_name] = value_element.string
 
         attributes["price"] = soup.find('span', attrs={'id': 'price'})
-        
-        meta_property_getter = self.meta_getter(soup).property_getter()
+
+        meta_property_getter = self.MetaGetter(soup).property
         meta_properties = [
             ("og:title", "name"),
             ("og:url", "url"),
@@ -59,7 +56,7 @@ class AutoTraderImporter(BaseImporter):
         ]
         attributes.update({key: meta_property_getter(property_name) for (property_name, key) in meta_properties})
 
-        meta_name_getter = self.meta_getter(soup).name_getter()
+        meta_name_getter = self.MetaGetter(soup).name
         meta_names = [
             ("description", "description")
         ]
